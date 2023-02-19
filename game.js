5158acd356721bc5f33d7942250af2845405151c
rakibc698@gmail.com <rakibc698@gmail.com>
2023-02-19 21:32:06 +0600

    commit add last time for my assignment 5

--- game.js
+++ game.js
@@ -1,12 +1,15 @@
 //triangle calculate
 document.getElementById('triangleBtn').addEventListener('click', function(){
-  const problem1 = calculateFunction('triangleInputField1');
-  const problem2 = calculateFunction('triangleInputField2');
-  const problem = 0.5 * problem1 * problem2;
+  const triangleInput1 = calculateFunction('triangleInputField1');
+  const triangleInput2 = calculateFunction('triangleInputField2');
+  const triangle = 0.5 * triangleInput1 * triangleInput2;
   
-  const last = showSection('trianglecalculateBtn');
+  //const update =
   
- setTexResult('trianglecalculateBtn', problem);
+  showSection('trianglecalculateBtn');
+  
+ setTexResult('trianglecalculateBtn', triangle);
+
 });
 
 //rectangle calculate
@@ -15,7 +18,9 @@ document.getElementById('rectangleBtn').addEventListener('click', function(){
   const inputField2of2 = calculateFunction('rectangleInputField2');
   const update = inputField1of1 * inputField2of2;
   
-  const last = showSection('ractangleCalculateBtn');
+  //const update = 
+  
+  showSection('ractangleCalculateBtn');
   
   setTexResult('ractangleCalculateBtn', update);
 })
@@ -26,7 +31,9 @@ document.getElementById('parallelogramBtn').addEventListener('click', function()
   const inputField2of2 = calculateFunction('parallelogramInputField2');
   const update = inputField1of1 * inputField2of2;
   
-  const last = showSection('parallelogramCalculateBtn');
+  //const update = 
+  
+  showSection('parallelogramCalculateBtn');
   
   setTexResult('parallelogramCalculateBtn', update);
 });
@@ -37,7 +44,9 @@ document.getElementById('rhombusBtn').addEventListener('click', function() {
   const inputField2of2 = calculateFunction('rhombusInputField2');
   const update = 0.5 * inputField1of1 * inputField2of2;
 
-  const last = showSection('rhombusCulateBtn');
+//const last = 
+  
+  showSection('rhombusCulateBtn');
 
   setTexResult('rhombusCulateBtn', update);
 });
@@ -49,24 +58,27 @@ document.getElementById('pentagonBtn').addEventListener('click', function() {
   const inputField2of2 = calculateFunction('pentagonInputField2');
   const update =0.5 * inputField1of1 * inputField2of2;
 
-  const last = showSection('pentagonCalculateBtn');
+  //const last =
+  showSection('pentagonCalculateBtn');
 
   setTexResult('pentagonCalculateBtn', update);
 });
 
 //Ellipse calculate
 document.getElementById('ellipseBtn').addEventListener('click', function() {
-  const inputField1of1 = calculateFunction('ellipseInputField');
-  const inputField2of2 = calculateFunction('ellipaseInputField2');
-  const update = 3.1416 * inputField1of1 * inputField2of2;
+  const inputField1of1 = calculateFunction('ellipseInputField1');
+  const inputField2of2 = calculateFunction('ellipseInputField2');
+  const update = 3.14 * inputField1of1 * inputField2of2;
 
-  const last = showSection('ellipaseCalculateBtn');
+  //const last = 
+  
+  showSection('ellipaseCalculateBtn');
 
   setTexResult('ellipaseCalculateBtn', update);
 });
 
-document.getElementById('locationChange').addEventListener('click', function(){
-  window.location.href = 'index1.html';
+document.getElementById('locationChangeBtn').addEventListener('click', function(){
+  window.location.href = 'index1.html'
 })
 
 
@@ -81,7 +93,7 @@ function calculateFunction(inputId){
 function showSection(elementId){
   const elementField = document.getElementById(elementId);
   const elementText = elementField.innerText;
-  const element = parseFloat(elementText);
+  const element = parseFloat(elementText).toFixed(2);
   return element;
 }
 function setTexResult(elementI , newV){
